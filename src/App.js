c5d17fa99f7a8f024f77beb7c2e8e91e0d018c66
Asif Khan <engrasif.khan521@gmail.com>
2021-07-15 13:18:05 -0700

    balance updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import { Header } from'./components/Header'
 import { Balance } from './components/Balance'
+import { AccountSummary } from './components/AccountSummary'
 function App() {
   return (
     <div>
       <Header />
       <div className="container">
         <Balance />
+        <AccountSummary />
       </div>
     </div>
   );
