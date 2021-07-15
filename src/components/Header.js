568219b03ee1448e582afc00791af8efe9536b1b
Asif Khan <engrasif.khan521@gmail.com>
2021-07-15 12:10:31 -0700

    comment out logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,12 @@ import React from 'react';
 export const Header = () => {
     return(
         <h1>
-        Expense Tracker By Asif Ayaz
-    
+
+           Expense Tracker By Asif-Ayazkhan
+           
         </h1>
+
+        
 
 
 
